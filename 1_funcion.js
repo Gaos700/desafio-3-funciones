de9dfd936c9eec35cdec9d7308fbe7a7ff48277d
Gaos700 <gustavorellana700@gmail.com>
2025-09-05 13:22:12 -0400

    ejercicio 1

---
+++ 1_funcion.js
@@ -0,0 +1,24 @@
+/*
+
+
+ ejercicio 1 funcion
+
+function example(a, b, c){
+    return a+b+c
+}
+console.log(example(2,3, 4)); 
+
+
+
+
+
+*/
+ 
+// la funcion pero como funcion de expresion
+
+
+const example1 = function(a,b,c) {
+    return a + b + c;
+};
+
+console.log(example1(3,4,5))
