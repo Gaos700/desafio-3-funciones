31043e1d9636fff18ec67e289443093838179fd5
G4os <gustavorellana700@gmail.com>
2025-09-06 21:25:39 -0400

    ejercio 3 terminado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,12 @@
-function pintar(){
-ele.style.backgroundColor = 'yellow'
+function pintar(evento, color = "yellow" ){
+    evento.target.style.backgroundColor = color;
 }
 const ele = document.getElementById("elem1")
-ele.addEventListener("click", pintar);
+
+const ele2 = document.getElementById("elem2")
+ele.addEventListener("click", function(event) {
+    pintar(event, 'yellow');
+});
+ele2.addEventListener("click", function(event) {
+    pintar(event, 'green'); 
+});
