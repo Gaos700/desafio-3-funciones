8fb003a1c3026875d674d4d98701266cdac19aa2
G4os <gustavorellana700@gmail.com>
2025-09-07 23:42:14 -0300

    ejerccio 4 inicio

---
+++ 4_colores.js
@@ -0,0 +1,5 @@
+['azul', 'rojo', 'verde', 'amarillo'].forEach(function(id){
+    document.getElementById(id).addEventListener('click', function(){
+        this.style.backgroundColor = 'black';
+    });
+});
