4c1362e9a80786ad2578f194e616635c18522adf
Gaos700 <gustavorellana700@gmail.com>
2025-09-08 14:20:13 -0300

    ejercicio 4 terminado, me demoré por que salí el domingo jaj

--- 4_colores.js
+++ 4_colores.js
@@ -3,3 +3,36 @@
         this.style.backgroundColor = 'black';
     });
 });
+let colorGlobal = "#fff"; 
+
+document.addEventListener('keydown', function(event){
+  if(event.key === 'a'){
+    colorGlobal = "pink";
+  }else if(event.key === 's'){
+    colorGlobal = "orange";
+  }else if(event.key === 'd'){
+    colorGlobal = "skyblue";
+  }else if(event.key === 'w'){
+    colorGlobal = "purple";
+  }else if(event.key === 'o'){
+    colorGlobal = "gray";
+  }else if(event.key === 'e'){
+    colorGlobal = "saddlebrown";
+  }
+
+  if(['a','s','d','w','o','e'].includes(event.key)){
+    let divKey = document.getElementById('key');
+    if(!divKey){
+      divKey = document.createElement('div');
+      divKey.id = "key";
+      divKey.style.width = "200px";
+      divKey.style.height = "200px";
+      divKey.style.display = "inline-block";
+      divKey.style.backgroundColor = colorGlobal;
+      divKey.style.color = "#fff";
+      document.body.appendChild(divKey);
+    }else{
+      divKey.style.backgroundColor = colorGlobal;
+    }
+  }
+});
